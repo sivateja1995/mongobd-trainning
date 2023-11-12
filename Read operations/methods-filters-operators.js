6c307a0d7aacf19815a5dccff5684b1a8f9db106
12sivateja <sivateja.d@sreenidhi1sports.com>
2023-11-12 23:46:28 +0530

    added the changes

---
+++ Read operations/methods-filters-operators.js
@@ -0,0 +1,32 @@
+db.getCollection("movies").find({});
+
+
+// methods for 
+
+db.movies.find({$and:[{"rating.average":{$gte:9}},{"genres":{$in:["Drama"]}}]});
+
+
+// short cut for the query
+db.movies.find({"rating.average":{$gte:9},"genres":{$in:["Drama"]}});
+
+
+// quering of the data where run time is not 60  min
+db.movies.find({runtime:{$not:{$eq:60}}}).count();
+
+// simplefied above query 
+db.movies.find({"runtime":{$ne:60}}).count();
+
+
+//diving into the element operators 
+show databases;
+use users;
+//* What are the element operators !? */
+db.users.insertMany([{name:'helium',hobbies:[{title:'internet surfing',frequency:6},{title:'cooking',frequency:6}],phone:"8008898808"},{name:'salvedor',hobbies:[{title:'football',frequency:4},{title:'treaking',frequency:1}],phone:"8008898909"}	]);
+
+// fetching of the users data 
+db.users.find(); 
+
+
+
+
+
---
+++ Read operations/notes.md
@@ -0,0 +1,12 @@
+# Read Operations
+
+1. using of the filters,operators & methods.
+2. accessing the data in the mongosh shell is given below
+
+    db.collectionName.find({age:32})
+
+    find ==> is the method.
+    {age:32} ===> is the paramter.
+
+3. Embedded querying of the fields and array.
+4. 
